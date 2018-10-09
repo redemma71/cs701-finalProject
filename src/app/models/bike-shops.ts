4498fd23f191b3328bf6648727f26cb0eab027d4
Chad David Cover <chad.cover@cengage.com>
2018-10-09 11:39:29 -0400

    setting up project

---
+++ src/app/models/bike-shops.ts
@@ -0,0 +1,20 @@
+export class BikeShops {
+
+  name:           string;
+  address:  {
+    street:       string;
+    city:         string;
+    state:        string;
+    zip:          string;
+  };
+  phone:          string;
+  hours:    {
+    monday:       string;
+    tuesday:      string;
+    wednesday:    string;
+    thursday:     string;
+    friday:       string;
+    saturday:     string;
+    sunday:       string;
+  };
+}
